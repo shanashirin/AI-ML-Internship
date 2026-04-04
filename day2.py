b51380d5e9a8b9943acf1b0a9258894edb897f03
fidhashiri <fidhashirinpulikkal02@gmail.com>
2026-04-04 13:02:17 +0530

    Added day2 notebook

---
+++ day2.py
@@ -0,0 +1,12 @@
+Name="Shana shirin p"
+Age=23
+Course="Python"
+print("Name:",Name)
+print("Age:",Age)
+print("Course:",Course)
+
+a=10
+b=5
+print("Addition:",a+b)
+print("Substraction:",a-b)
+print("Multiply:",a*b)
